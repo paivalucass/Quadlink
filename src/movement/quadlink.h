a2f1ae33ef74b3ca67120d1cc24ff58bab441453
paivalucass <lucaspaiva2805@gmail.com>
2024-08-21 00:03:50 -0300

    Add QuadCopter connection method

--- src/movement/quadlink.h
+++ src/movement/quadlink.h
@@ -7,27 +7,31 @@
 #include <mavsdk/plugins/action/action.h>
 #include <mavsdk/plugins/telemetry/telemetry.h>
 
-class QuadCopter {
-public:
-    // Construtor que inicializa o drone com uma configuração específica
-    QuadCopter(const mavsdk::Mavsdk::Configuration& config);
+namespace quadlink {
 
-    // Método para conectar o drone via URL (pode ser UDP, TCP, etc.)
-    bool connect(const std::string& connection_url);
+    class QuadCopter {
+    public:
+        // Construtor que inicializa o drone com uma configuração específica
+        QuadCopter(const mavsdk::Mavsdk::Configuration& config);
 
-    // Método para iniciar o voo
-    void takeoff();
+        // Método para conectar o drone via URL (pode ser UDP, TCP, etc.)
+        bool connect(const std::string& connection_url);
 
-    // Método para pousar o drone
-    void land();
+        // Método para iniciar o voo
+        void takeoff(double height);
 
-    // Método para obter o nível de bateria atual
-    void get_battery_status();
+        // Método para pousar o drone
+        void land();
 
-private:
-    // Instâncias do MAVSDK, Action e Telemetry
-    mavsdk::Mavsdk mavsdk;
-    std::shared_ptr<mavsdk::Action> action;
-    std::shared_ptr<mavsdk::Telemetry> telemetry;
-};
+        // Método para obter o nível de bateria atual
+        void get_battery_status();
+
+    private:
+        // Instâncias do MAVSDK, Action e Telemetry
+        mavsdk::Mavsdk mavsdk;
+        std::shared_ptr<mavsdk::Action> action;
+        std::shared_ptr<mavsdk::Telemetry> telemetry;
+    };
+
+}
 
