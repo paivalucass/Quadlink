a53bafc18eb6b826fdcba3093cd118aa96cf40d4
paivalucass <lucaspaiva2805@gmail.com>
2024-08-24 14:53:36 -0300

    Add full connection via UDP

--- src/connection/quad_connection.cpp
+++ src/connection/quad_connection.cpp
@@ -33,14 +33,47 @@ quadlink::ConnectionStatus quadlink::QuadConnector::connect_udp(std::string& con
     
     quadlink::QuadConnector::sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-    if (QuadConnector::sockfd < 0)
+    if (quadlink::QuadConnector::sockfd < 0)
     {
+        close(quadlink::QuadConnector::sockfd);
         cerr << "Failed creating socket" << endl;
+        return quadlink::ConnectionStatus::Failed;
     }
 
-    QuadConnector::server_addr.sin_family = AF_INET;
-    QuadConnector::server_addr.sin_port = htons(port);
-    QuadConnector::server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // Bind to all interfaces
+    quadlink::QuadConnector::server_addr.sin_family = AF_INET;
+    quadlink::QuadConnector::server_addr.sin_port = htons(port);
+    quadlink::QuadConnector::server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // Bind to all interfaces
 
+    if (bind(quadlink::QuadConnector::sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        close(quadlink::QuadConnector::sockfd);
+        cerr << "Failed binding socket" << endl;
+        return quadlink::ConnectionStatus::Failed;
+    }
 
+    uint8_t buffer[2048];
+    sockaddr_in drone_addr;
+    socklen_t addr_len = sizeof(drone_addr);
+
+    ssize_t len = recvfrom(quadlink::QuadConnector::sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&drone_addr, &addr_len);
+
+    if (len < 0) {
+        close(sockfd);
+        return quadlink::ConnectionStatus::Failed;
+    } else {
+        cout << "Message received, length: " << len << endl;
+        mavlink_message_t msg;
+        mavlink_status_t status;
+        // Decode the MAVLink message
+        for (ssize_t i = 0; i < len; ++i) {
+            if (mavlink_parse_char(MAVLINK_COMM_0, buffer[i], &msg, &status)) {
+                if (msg.msgid == MAVLINK_MSG_ID_HEARTBEAT) {
+                    close(sockfd);
+                    return quadlink::ConnectionStatus::Success;
+                }
+            }
+        }
+    }
+
+    close(sockfd);
+    return quadlink::ConnectionStatus::Failed;
 }
