50208eaaa52db155de29ecfe7aa24ce8d7ff50a7
paivalucass <lucaspaiva2805@gmail.com>
2024-08-30 21:34:57 -0300

    Add telemetry sensors monitoring method

--- src/telemetry/quad_telemetry.h
+++ src/telemetry/quad_telemetry.h
@@ -1,6 +1,7 @@
 #include <string>
 #include <memory>
 #include <mavlink/common/mavlink.h>
+#include "../connection/message_factory.h"
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
@@ -12,12 +13,36 @@
 
 
 namespace quadlink{
+    enum class SensorStatus{
+        Present,
+        Absent,
+        Enabled,
+        Disabled,
+        Helthy,
+        Unhelthy
+    };
+
+    typedef struct {
+        SensorStatus gyro;
+        SensorStatus accel;
+        SensorStatus mag;
+        SensorStatus gps;
+    } Sensors;
+
     class QuadTelemetry : public virtual quadlink::QuadConnector{
         public:
             QuadTelemetry();
 
             ~QuadTelemetry() = default;
 
+            quadlink::Sensors check_sensors_health();
+
+            /*
+            Perfoms a bitmask AND to verify sensor status
+            */
+            quadlink::SensorStatus sensor_health(mavlink_sys_status_t sys, uint8_t sensor);
+
         private:
+            std::shared_ptr<quadlink::MessageFactory> msg_factory;
     };
 }
