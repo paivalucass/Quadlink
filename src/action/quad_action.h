c7bc860e498e1b29dcbfacf73713374914dc7331
paivalucass <lucaspaiva2805@gmail.com>
2024-09-12 11:03:31 -0300

    Add documentation to message factory and action module

--- src/action/quad_action.h
+++ src/action/quad_action.h
@@ -13,22 +13,56 @@
 
 
 namespace quadlink{
+    /**
+     * @brief A class for performing actions on a quadcopter.
+     */
     class QuadAction : public virtual quadlink::QuadConnector{
         public:
+            /**
+             * @brief Constructor for QuadAction.
+             */
             QuadAction();
 
+            /**
+             * @brief Destructor for QuadAction.
+             */
             ~QuadAction() = default;
 
+            /**
+             * @brief Arms the quadcopter.
+             * @return The status of the connection.
+             */
             quadlink::ConnectionStatus action_arm();
 
+            /**
+             * @brief Changes the flight mode of the quadcopter.
+             * @param flight_mode The desired flight mode.
+             * @return The status of the connection.
+             */
             quadlink::ConnectionStatus action_change_mode(ArdupilotFlightMode flight_mode);
 
+            /**
+             * @brief Sets the home position of the quadcopter.
+             * @return The status of the connection.
+             */
             quadlink::ConnectionStatus action_set_home_position();
 
+            /**
+             * @brief Initiates takeoff of the quadcopter.
+             * @return The status of the connection.
+             */
             quadlink::ConnectionStatus action_takeoff();
 
+            /**
+             * @brief Initiates landing of the quadcopter.
+             * @return The status of the connection.
+             */
             quadlink::ConnectionStatus action_land();
             
+            /**
+             * @brief Sets the takeoff height of the quadcopter.
+             * @param height The desired takeoff height.
+             */
             void action_set_takeoff_height(float height);
 
         private:
