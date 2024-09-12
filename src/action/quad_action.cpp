d8bc4326b60e2d54dbb68e0b2cc96b4dfb242fac
paivalucass <lucaspaiva2805@gmail.com>
2024-09-12 17:00:54 -0300

    Add SET_POSITION_TARGET_LOCAL_NED message command and message building.

--- src/action/quad_action.cpp
+++ src/action/quad_action.cpp
@@ -47,9 +47,45 @@ quadlink::ConnectionStatus quadlink::QuadAction::action_land(){
     // TODO: Implement action land (actually needed?)
 }
 
-quadlink::ConnectionStatus quadlink::QuadAction::action_set_position_target_local_ned(uint16_t type_mask, float x, float y, float z, float vx = 0, float vy = 0, float vz = 0, float ax = 0, float ay = 0, float az = 0, float yaw = 0, float yaw_rate = 0){
+quadlink::ConnectionStatus quadlink::QuadAction::action_set_position_target_local_ned(float x, float y, float z, float vx = 0, float vy = 0, float vz = 0, float ax = 0, float ay = 0, float az = 0, float yaw = 0, float yaw_rate = 0, uint16_t type_mask = 0b0000110111111000){
+    // default action is based only in position
+    mavlink_set_position_target_local_ned_t target_local_ned_command = msg_factory->create_set_local_ned_command(type_mask, 
+                                                                                                                 x, 
+                                                                                                                 y, 
+                                                                                                                 z, 
+                                                                                                                 vx, 
+                                                                                                                 vy, 
+                                                                                                                 vz, 
+                                                                                                                 ax, 
+                                                                                                                 ay, 
+                                                                                                                 az, 
+                                                                                                                 yaw, 
+                                                                                                                 yaw_rate);
 
+    mavlink_message_t msg = this->build_command(target_local_ned_command);                                                                                  
+
+    return quadlink::QuadConnector::send_mav_message(msg);
 }
+
+quadlink::ConnectionStatus quadlink::QuadAction::action_set_position_target_body(float x, float y, float z, float vx = 0, float vy = 0, float vz = 0, float ax = 0, float ay = 0, float az = 0, float yaw = 0, float yaw_rate = 0, uint16_t type_mask = 0b0000110111111000){
+    mavlink_set_position_target_local_ned_t target_body_command = msg_factory->create_set_body_command(type_mask, 
+                                                                                                                 x, 
+                                                                                                                 y, 
+                                                                                                                 z, 
+                                                                                                                 vx, 
+                                                                                                                 vy, 
+                                                                                                                 vz, 
+                                                                                                                 ax, 
+                                                                                                                 ay, 
+                                                                                                                 az, 
+                                                                                                                 yaw, 
+                                                                                                                 yaw_rate);
+
+    mavlink_message_t msg = this->build_command(target_body_command);                                                                                  
+
+    return quadlink::QuadConnector::send_mav_message(msg);
+}
+
 
 void quadlink::QuadAction::action_set_takeoff_height(float height){
 
--- src/connection/message_factory.h
+++ src/connection/message_factory.h
@@ -80,7 +80,13 @@ namespace quadlink{
              * @brief Creates a set local NED command message.
              * @return The created message.
              */
-            mavlink_set_position_target_local_ned_t create_set_local_ned_command();
+            mavlink_set_position_target_local_ned_t create_set_local_ned_command(uint16_t type_mask, float x, float y, float z, float vx, float vy, float vz, float ax, float ay, float az, float yaw, float yaw_rate);
+            
+            /**
+             * 
+            */
+            mavlink_set_position_target_local_ned_t create_set_body_command(uint16_t type_mask, float x, float y, float z, float vx, float vy, float vz, float ax, float ay, float az, float yaw, float yaw_rate);
+
 
         private:
 
