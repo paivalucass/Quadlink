cb52517e19de8ba18d70a100d5f8b088e56303a6
paivalucass <lucaspaiva2805@gmail.com>
2024-10-05 14:26:39 -0300

    Add Qt base files and cmakelist submodule

---
+++ src/gui/build/Desktop_Qt_6_7_3-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/home/lucas/Qt/6.7.3/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/home/lucas/Qt/6.7.3/gcc_64" CACHE "PATH" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/lucas/RCPILOTC/Quadlink/src/gui/build/Desktop_Qt_6_7_3-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/lib/ccache/clang-14" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/lib/ccache/clang++-14" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
